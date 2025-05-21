721d8d7e47b0bbb9a8d27c17901a00ca68aac090
Luis Angel Perez Mariscal <legends15231@gmail.com>
2025-05-21 07:39:18 -0600

    localStorage

--- src/js/dataManager.js
+++ src/js/dataManager.js
@@ -1,3 +1,48 @@
+// Almacenamiento en local storage
+export default class DataManager {
+    constructor(keyStorage) {
+        this.keyStorage = keyStorage;
+        this.dbStorage = JSON.parse(localStorage.getItem(this.keyStorage)) || [];
+    }
+
+    // CREATE
+    createData(objContribucion) {
+        this.dbStorage.push(objContribucion);
+        localStorage.setItem(this.keyStorage, JSON.stringify(this.dbStorage));
+    }
+
+    // READ
+    readData() {
+        return this.dbStorage;
+    }
+
+    // UPDATE
+    updateData(id, nuevaContribucion) {
+        this.dbStorage = this.dbStorage.map((contribucion) => {
+            if (contribucion.id === id) {
+                return { ...contribucion, ...nuevaContribucion };
+            }
+            return contribucion;
+        });
+        localStorage.setItem(this.keyStorage, JSON.stringify(this.dbStorage));
+    }
+
+    // DELETE
+    deleteData(id) {
+        this.dbStorage = this.dbStorage.filter((contribucion) => contribucion.id !== id);
+        localStorage.setItem(this.keyStorage, JSON.stringify(this.dbStorage));
+    }
+
+    // CLEAR
+    clearData() {
+        this.dbStorage = [];
+        localStorage.removeItem(this.keyStorage);
+        this.dbStorage = JSON.parse(localStorage.getItem(this.keyStorage)) || [];
+    }
+}
+
+/*
+ALMACENAMIOENTO EN SESSION STORAGE
 export default class DataManager {
     constructor(keySession) {
         this.keySession = keySession;
@@ -38,4 +83,4 @@ export default class DataManager {
         sessionStorage.removeItem(this.keySession);
         this.dbSession = JSON.parse(sessionStorage.getItem(this.keySession)) || [];
     }
-}
+}*/
